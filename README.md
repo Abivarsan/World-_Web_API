efb85a85e2a5598184bd563ff954bfd0ee54343f
Abivarsan <ketheesabi7400@gmail.com>
2025-02-10 22:23:35 +0530

    sample .net core project created

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+# World_Web_API
+Sample ASP.NET Core Web API Project
+=======
+# World
+>>>>>>> f70683e (sample web api project created)
---
+++ World.Api/Controllers/CountryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using World.Api.Data;
+
+namespace World.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CountryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+    }
+}
