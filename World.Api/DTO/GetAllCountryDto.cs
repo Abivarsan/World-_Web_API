2a09ac652a6fa3affbaa55ef81ab3b834db0cf97
Abivarsan <ketheesabi7400@gmail.com>
2025-02-11 22:32:15 +0530

    create,update,get all Dtos created

---
+++ World.Api/DTO/GetAllCountryDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace World.Api.DTO
+{
+    public class GetAllCountryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
