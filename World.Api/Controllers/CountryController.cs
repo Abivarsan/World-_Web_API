9af855e91f9402340db6112a30551dd1a31883b3
Abivarsan <ketheesabi7400@gmail.com>
2025-02-11 10:47:09 +0530

    basic CRUD operations finished

--- World.Api/Controllers/CountryController.cs
+++ World.Api/Controllers/CountryController.cs
@@ -9,25 +9,48 @@ namespace World.Api.Controllers
     [ApiController]
     public class CountryController : ControllerBase
     {
-        private readonly ApplicationDbContext _dbcontext;
+        private readonly ApplicationDbContext _dbContext;
         public CountryController(ApplicationDbContext dbcontext)
         {
-            _dbcontext = dbcontext;
+            _dbContext = dbcontext;
         }
 
         [HttpPost]
         public ActionResult<Country> Create([FromBody] Country country)
         {
-            _dbcontext.Countries.Add(country);
-            _dbcontext.SaveChanges();
+            _dbContext.Countries.Add(country);
+            _dbContext.SaveChanges();
             return Ok();
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Country>> GetAll()
         {
-            return _dbcontext.Countries.ToList();
+            return _dbContext.Countries.ToList();
         }
 
+        [HttpGet("{id:int}")]
+        public ActionResult<Country> GetById(int id)
+        {
+            return _dbContext.Countries.Find(id);
+        }
+
+        [HttpPut]
+        public ActionResult<Country> Update([FromBody] Country country)
+        {
+            _dbContext.Countries.Update(country);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+
+        public ActionResult<Country> DeleteById(int id)
+        {
+            var country = _dbContext.Countries.Find(id);
+            _dbContext.Countries.Remove(country);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
     }
 }
